03e9308c9b3a212cd4b9eb483a20e3a1c25407a7
stevending1st <stevending1st@users.noreply.github.com>
2023-04-22 12:35:06 +0000

    deploy: 34c0ac20cc0232c82b6b7c3e24883c677baff8cc

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Vite-React-MobX-Bootstrap-ts/sw.js', { scope: '/Vite-React-MobX-Bootstrap-ts/' })})}
